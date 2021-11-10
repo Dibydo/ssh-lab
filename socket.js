a309ba5da024aa58e8c8ece5999ab67345897bb8
Dibydo <yaroslav.zhovtyak@gmail.com>
2021-11-10 17:14:22 +0300

    HTML page and JS socket handle

---
+++ socket.js
@@ -0,0 +1,39 @@
+window.addEventListener("load", function(){
+    this.alert("load")
+    var form2 = document.getElementById("form2")
+    connectBut = this.document.getElementById("connect")
+    connectBut.addEventListener("click", function() {
+        connectBut.style.display = "none"
+        var socket = new WebSocket("ws://127.0.0.1:6060/ws");
+        socket.onopen = function() {
+            document.getElementById("form-container2").style.display = "block"
+          };
+          
+          socket.onclose = function(event) {
+            if (event.wasClean) {
+              alert('Closed clean');
+            } else {
+              alert('Closed bad');
+            }
+            alert('Code: ' + event.code + '. Reason: ' + event.reason);
+            connectBut.style.display = "inline"
+            document.getElementById("form-container2").style.display = "none"
+          };
+          
+          socket.onmessage = function(event) {
+            let answer = document.getElementById("answer")
+            answer.innerHTML = event.data
+          };
+          
+          socket.onerror = function(error) {
+            alert("Error " + error.message);
+          };
+          let command = document.getElementById("command")
+          command.value = ""
+          form2.addEventListener("submit", function(event){
+            event.preventDefault();
+            socket.send(command.value)
+          })
+    })
+})  
+
